9bc7de8de8efd51b753ef9cd37e91d663b97d1fa
wolfbook22 <wolfbook22@hotmail.com>
2025-04-23 17:29:58 -0400

    Add files via upload

---
+++ NonProfitDrop.sql
@@ -0,0 +1,7 @@
+DROP TABLE `jknuth1db`.`Donor`;
+DROP TABLE `jknuth1db`.`Event`;
+DROP TABLE `jknuth1db`.`Donation`;
+DROP TABLE `jknuth1db`.`Volunteer`;
+DROP TABLE `jknuth1db`.`Beneficiary`;
+DROP TABLE `jknuth1db`.`Volunteer_At`;
+DROP TABLE `jknuth1db`.`Expenses`;
